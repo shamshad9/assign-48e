c5cd82f5b9de227603a9676fda6e5aad93b380fa
shamshad <shamshad.cheema@gmail.com>
2023-08-17 06:00:02 -0700

    this is my first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+/*let message = 'Hello world';
+
+console.log(message);*/
+let day = [0, 'saturday'];
+console.log(day[0]);
+console.log(day[1]);
+day[2];
+day[3];
---
+++ index.ts
@@ -0,0 +1,13 @@
+
+/*let message = 'Hello world';
+
+console.log(message);*/
+let  day: any  = [ 0 ,'saturday']
+
+console.log(day[0]);
+console.log(day[1]);
+day[2]
+day[3]clear
+import chalk from 'chalk'
+console.log "import chalk
+"
